d292e1494842a7964511f7b0dcf3e3f8f76731b2
rahmadestriani <rahma.destriani03@gmail.com>
2020-11-29 15:09:52 +0700

    update model

---
+++ app/src/main/java/com/example/wisnu/model/RootPariwisataModel.java
@@ -0,0 +1,18 @@
+package com.example.wisnu.model;
+
+import java.util.ArrayList;
+import com.google.gson.annotations.SerializedName;
+
+public class RootPariwisataModel{
+
+    @SerializedName("wisata")
+    private ArrayList<WisataItem> wisata;
+
+    public void setWisata(ArrayList<WisataItem> wisata){
+        this.wisata = wisata;
+    }
+
+    public ArrayList<WisataItem> getWisata(){
+        return wisata;
+    }
+}
